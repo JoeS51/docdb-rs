99a6a778defb8874988a5e7af5557c19335b6f22
Johannes Sluis <joesluis51@gmail.com>
2026-04-21 23:19:29 -0700

    add small unit tests

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Goal: Build a document database from scratch similar to Azure Cosmos DB.
 ### Design doc
 
 Rough Implementation Plan:
-- [ ] In memory store (just use basic hashmap)
+- [x] In memory store (just use basic hashmap)
 - [ ] Naive file persistence by putting the whole hashmap into a file and deserializing on startup
 - [ ] Append only log
 - [ ] Pages and heap file
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
         collection: "collection1".to_string(),
         id: "id1".to_string(),
     };
-    add_document(&mut db, &doc_key, john);
+    add_document(&mut db, &doc_key, &john);
     println!("Insert document: {:?}", db);
 
     let result = get_document(&db, &doc_key);
@@ -38,9 +38,9 @@ fn main() {
 fn add_document(
     db: &mut HashMap<DocumentKey, serde_json::Value>,
     key: &DocumentKey,
-    value: serde_json::Value,
+    value: &serde_json::Value,
 ) {
-    db.insert(key.clone(), value);
+    db.insert(key.clone(), value.clone());
 }
 
 fn get_document<'a>(
@@ -52,4 +52,44 @@ fn get_document<'a>(
 
 fn delete_document(db: &mut HashMap<DocumentKey, serde_json::Value>, key: &DocumentKey) {
     db.remove(key);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_document() {
+        let mut db: HashMap<DocumentKey, serde_json::Value> = HashMap::new();
+        let val = json!({
+            "name": "John Doe",
+            "age": 43,
+        });
+        let doc_key = DocumentKey {
+            partition_key: "partition1".to_string(),
+            collection: "collection1".to_string(),
+            id: "id1".to_string(),
+        };
+        add_document(&mut db, &doc_key, &val);
+        let get_result = get_document(&db, &doc_key);
+        assert_eq!(&val, get_result.unwrap());
+    }
+
+    #[test]
+    fn test_delete_document() {
+        let mut db: HashMap<DocumentKey, serde_json::Value> = HashMap::new();
+        let val = json!({
+            "name": "John Doe",
+            "age": 43,
+        });
+        let doc_key = DocumentKey {
+            partition_key: "partition1".to_string(),
+            collection: "collection1".to_string(),
+            id: "id1".to_string(),
+        };
+        add_document(&mut db, &doc_key, &val);
+        delete_document(&mut db, &doc_key);
+        let get_result = get_document(&db, &doc_key);
+        assert_eq!(None, get_result);
+    }
 }
