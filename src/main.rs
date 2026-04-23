8596faed6ef07a958ad449b6d076a4a4d11f9f3a
Joe Sluis <joesluis51@gmail.com>
2026-04-22 23:39:01 -0700

    Start persisting documents to disk

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+use serde::{Deserialize, Serialize};
 use serde_json;
 use serde_json::json;
 use std::collections::HashMap;
+use std::collections::hash_map::Keys;
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::fs::{self, File};
 use std::io;
 use std::io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write};
 
-#[derive(Eq, Hash, PartialEq, Debug, Clone)]
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Serialize, Deserialize)]
 struct DocumentKey {
     partition_key: String,
     collection: String,
     id: String,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Serialize, Deserialize)]
+enum LogEntry {
+    Add {
+        key: DocumentKey,
+        value: serde_json::Value,
+    },
+    Delete {
+        key: DocumentKey,
+    },
 }
 
 struct Database {
@@ -24,9 +37,17 @@ impl Database {
         let db_file = OpenOptions::new()
             .create(true)
             .append(true)
+            .read(true)
             .open("db.log")
             .unwrap(); // consider removing this unwrap if this is bad practice
         let map: HashMap<DocumentKey, serde_json::Value> = HashMap::new();
+
+        // let reader = BufReader::new(&db_file);
+        // for line in reader.lines() {
+        //     let line = line.unwrap(); // potentially fix unwrap
+        //     let parts: Vec<&str> = line.split(':').collect();
+        //     println!("{:?}", parts);
+        // }
 
         Database {
             documents: map,
@@ -35,7 +56,14 @@ impl Database {
     }
 
     fn add_document(db: &mut Self, key: &DocumentKey, value: &serde_json::Value) {
+        let key = key.clone();
+        let value = value.clone();
         db.documents.insert(key.clone(), value.clone());
+        let log_entry = LogEntry::Add { key, value };
+        // TODO: get rid of unwrap
+        serde_json::to_writer(&mut db.commit_log, &log_entry).unwrap();
+        db.commit_log.write_all(b"\n").unwrap();
+        db.commit_log.flush().unwrap();
     }
 
     fn get_document<'a>(db: &'a mut Self, key: &DocumentKey) -> Option<&'a serde_json::Value> {
@@ -43,7 +71,13 @@ impl Database {
     }
 
     fn delete_document(db: &mut Self, key: &DocumentKey) {
-        db.documents.remove(key);
+        let key = key.clone();
+        db.documents.remove(&key);
+        let log_entry = LogEntry::Delete { key: key };
+        // TODO: get rid of unwrap
+        serde_json::to_writer(&mut db.commit_log, &log_entry).unwrap();
+        db.commit_log.write_all(b"\n").unwrap();
+        db.commit_log.flush().unwrap();
     }
 }
 
