1b3f5736ab92b6e9c14ffbaf09249b3b5e026e3f
Joe Sluis <joesluis51@gmail.com>
2026-05-10 22:05:40 -0700

    start scaffolding lsm storage engine implementation

--- src/storage_engine/lsm.rs
+++ src/storage_engine/lsm.rs
@@ -1,16 +1,29 @@
 // LSM-based storage engine
 use super::{StorageEngine, StorageError};
+use std::collections::BTreeMap;
+use std::path::Path;
+use std::fs::{self, File};
 
-pub struct LSM {
-    map: HashMap<Vec<u8>, Vec<u8>>,
+enum ValueEntry {
+    Put(Vec<u8>),
+    Tombstone,
 }
 
-impl MemoryStorage {
+struct Sstable {
+    file_path: Path,
+}
+
+pub struct LsmStorage {
+    mem_table: BTreeMap<Vec<u8>, ValueEntry>,
+    sstables: Vec<SsTable>,
+}
+
+impl LsmStorage {
     pub fn new() -> Self {
     }
 }
 
-impl StorageEngine for MemoryStorage {
+impl StorageEngine for LsmStorage {
     fn put(&mut self, key: Vec<u8>, value: Vec<u8>) -> Result<(), StorageError> {
     }
     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, StorageError> {
